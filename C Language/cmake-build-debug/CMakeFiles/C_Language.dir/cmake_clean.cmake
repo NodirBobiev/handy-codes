2a6817bf6544141144a1e9bd66b71b172e8e19d7
NodirBobiev <nodir.bobiev@gmail.com>
2021-09-25 23:49:55 +0300

    Add 'C Language' folder

---
+++ C Language/cmake-build-debug/CMakeFiles/C_Language.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Language.dir/main.c.obj"
+  "C_Language.exe"
+  "C_Language.exe.manifest"
+  "C_Language.pdb"
+  "libC_Language.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Language.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
